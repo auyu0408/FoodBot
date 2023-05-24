a029ca093ffcf401c72d7d29ade599336581fdd3
hbr890627 <hbr890627@gmail.com>
2023-05-25 01:40:56 +0800

    Update TDD

--- test_foodAPI_other.py
+++ test_foodAPI_other.py
@@ -5,4 +5,44 @@ from test_foodAPI import DEFAULT
 
 
 class TestOther(TestFoodAPI):
-    pass
+    def test_set_location(self):
+        longitude = DEFAULT.longitude
+        latitude = DEFAULT.latitude
+        location = DEFAULT.location
+
+        self.bot.set_location(longitude=longitude, latitude=latitude)
+        self.assertAlmostEqual(self.bot.longitude, longitude, delta=0.01)
+        self.assertAlmostEqual(self.bot.latitude, latitude, delta=0.01)
+
+        # self.bot.set_location(location=location)
+        # self.assertEqual(self.bot.location, location)
+        # self.assertEqual(self.bot.longitude, longitude)
+        # self.assertEqual(self.bot.latitude, latitude)
+
+    def test_get_resturaunts(self):
+        ls = self.bot.get_restaurants()
+        excepted_ls = {'李記', '魯肉飯', '鍋燒麵', '麥當勞', '牛排館'}
+        self.assertEqual(ls, excepted_ls)
+
+    def test_filter(self):
+        # defaut food preference
+        results = self.bot.get_restaurants()
+        ls = self.bot.filter(results)
+        excepted_ls = {'李記', '魯肉飯', '鍋燒麵'}
+        self.assertEqual(ls, excepted_ls)
+
+        # after adding food preference
+        self.bot.add_food_preference('牛排')
+        ls = self.bot.filter(results)
+        excepted_ls = {'李記', '魯肉飯', '鍋燒麵', '牛排館'}
+        self.assertEqual(ls, excepted_ls)
+
+    def test_recommend(self):
+        results = self.bot.get_restaurants()
+        ls = self.bot.recommend(results)
+        excepted = '李記'
+        self.assertEqual(ls, excepted)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_foodAPI_price_budget.py
@@ -0,0 +1,19 @@
+import unittest
+
+from test_foodAPI import TestFoodAPI
+from test_foodAPI import DEFAULT
+
+
+class TestPriceBudget(TestFoodAPI):
+    def test_add_price_budget(self):
+        pass
+
+    def test_reset_price_budget(self):
+        pass
+
+    def test_get_price_budget(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
