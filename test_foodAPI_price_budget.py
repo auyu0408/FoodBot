e07483fd02b302c5e3eba2a612c9c925a828e5b5
hbr890627 <hbr890627@gmail.com>
2023-05-25 14:03:34 +0800

    Update tests for price budget function

--- test_foodAPI_price_budget.py
+++ test_foodAPI_price_budget.py
@@ -6,13 +6,28 @@ from test_foodAPI import DEFAULT
 
 class TestPriceBudget(TestFoodAPI):
     def test_add_price_budget(self):
-        pass
+        expected = {'低'}
+        ret = self.bot.add_price_budget('低')
+        self.assertEqual(ret, expected)
+
+        expected = {'低', '中'}
+        ret = self.bot.add_price_budget('中')
+        self.assertEqual(ret, expected)
+
+        expected = {'低', '中', '高'}
+        ret = self.bot.add_price_budget('高')
+        self.assertEqual(ret, expected)
 
     def test_reset_price_budget(self):
-        pass
+        ret = self.bot.reset_price_budget()
+        self.assertEqual(ret, set())
 
     def test_get_price_budget(self):
-        pass
+        expected = self.bot.price_budget
+        ret = self.bot.get_price_budget()
+
+        for a, b in zip(ret, expected):
+            self.assertEqual(a, b)
 
 
 if __name__ == '__main__':
